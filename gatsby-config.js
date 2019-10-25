af080230e126fc5c9ea426a8981f6ac9d3182a2d
Maggie <maggie@egghead.io>
2019-10-25 14:47:59 +0100

    still broken

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/content/metaposts/`,
+        path: `${__dirname}/content/meta/`,
         name: 'meta',
         ignore: [`**/\.*`], // ignore files starting with a dot,
       },
